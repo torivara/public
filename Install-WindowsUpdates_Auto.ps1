f36bef467a433db55c8dd328270ed97f3ef4ade4
Tor Ivar Asbølmo <torivara@gmail.com>
2018-06-29 13:04:56 +0200

    Update Install-WindowsUpdates_Auto.ps1

--- Install-WindowsUpdates_Auto.ps1
+++ Install-WindowsUpdates_Auto.ps1
@@ -1,4 +1,6 @@
-# Script installs NuGet and suppresses confirm dialogs. NuGet needed for Install-Module
+# WARNING! This script will automatically reboot computer if necessary.
+# Use with caution!
+# Install NuGet and suppress confirmation. NuGet needed for Install-Module
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -Confirm:$false | Out-Null
 
 # Make sure repository name PSGallery is trusted
@@ -7,7 +9,7 @@ Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted -Confirm:$false
 # Install Windows Update module from PSGallery
 Install-Module PSWindowsUpdate -Force -Confirm:$false
 
-# Add Microsoft Update
+# Add Microsoft Update Service
 Add-WUServiceManager -ServiceID 7971f918-a847-4430-9279-4a52d1efe18d -Confirm:$false | Out-Null
 
 # Fetch and install updates, automatic reboot
