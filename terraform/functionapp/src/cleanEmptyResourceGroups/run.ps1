6ea3ece48c8548fa3e5490433f0584839cee646f
Tor Ivar <torivara@gmail.com>
2022-09-02 14:07:02 +0200

    Adds function app code

---
+++ terraform/functionapp/src/cleanEmptyResourceGroups/run.ps1
@@ -0,0 +1,17 @@
+# Input bindings are passed in via param block.
+param($Timer)
+
+Connect-AzAccount -Identity
+
+Get-AzResourceGroup
+
+$AllRGs = (Get-AzResourceGroup).ResourceGroupName
+$UsedRGs = (Get-AzResource | Group-Object ResourceGroupName).Name
+$EmptyGroups = $AllRGs | Where-Object {$_ -notin $UsedRGs}
+
+# Write out empty resource groups
+Write-Output "These resource groups are empty:"
+
+Foreach ($group in $EmptyGroups) {
+    Write-Output "- $group"
+}
