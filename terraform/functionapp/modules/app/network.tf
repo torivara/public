1e6cf65fe1b27f2f69118c25f89d0582024489af
Tor Ivar <torivara@gmail.com>
2022-09-07 13:54:49 +0200

    Update

--- terraform/functionapp/modules/app/network.tf
+++ terraform/functionapp/modules/app/network.tf
@@ -8,7 +8,7 @@ resource "azurerm_virtual_network" "integration-vnet" {
 
 resource "azurerm_subnet" "integration-subnet" {
   count                = var.vnet_integration_enabled == true && var.vnet_integration_subnet_id == null ? 1 : 0
-  address_prefixes     = ["10.0.0.128/25"]
+  address_prefixes     = ["10.0.0.0/25"]
   name                 = "integrationSubnet"
   resource_group_name  = azurerm_resource_group.fa-rg.name
   virtual_network_name = azurerm_virtual_network.integration-vnet[0].name
