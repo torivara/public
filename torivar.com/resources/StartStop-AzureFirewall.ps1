3fd490211536db416e84136efc064fcd178ffde8
Tor Ivar <torivara@gmail.com>
2023-03-10 10:04:43 +0100

    updates script

--- torivar.com/resources/StartStop-AzureFirewall.ps1
+++ torivar.com/resources/StartStop-AzureFirewall.ps1
@@ -40,24 +40,38 @@ if (-not (Get-Module -Name "Az.Network")) {
 #>
 $context = Get-AzContext
 if (-not $context.Tenant) {
+  Write-Output "Logging in."
   if ($managedIdentity -ne "") {
     Connect-AzAccount -Identity
   } else {
     Connect-AzAccount
   }
+  Write-Output "Switching to correct subscription."
   Set-AzContext -Subscription $subscription
 }
 
 # Get the firewall object into a variable
+Write-Output "Loading firewall info."
 $azfw = Get-AzFirewall -Name $fwName -ResourceGroupName $rgName
 
 # Process firewall allocate or deallocate
 if ($mode -eq "deallocate" -and $azFw) {
-  $azfw.Deallocate()
-  Set-AzFirewall -AzureFirewall $azfw
+  if ($azFw.IpConfigurations.Count -gt 0) {
+    Write-Output "Deallocating firewall."
+    $azfw.Deallocate()
+    Set-AzFirewall -AzureFirewall $azfw
+  } else {
+    Write-Output "Firewall already deallocated."
+  }
+  
 } elseif ($mode -eq "allocate" -and $azFw) {
-  $vnet = Get-AzVirtualNetwork -ResourceGroupName $rgName -Name $vnetName
-  $pip = Get-AzPublicIpAddress -ResourceGroupName $rgName -Name $pipName
-  $azFw.Allocate($vnet, $pip)
-  Set-AzFirewall -AzureFirewall $azfw
+  if ($azFw.IpConfigurations.Count -eq 0) {
+    Write-Output "Allocating firewall."
+    $vnet = Get-AzVirtualNetwork -ResourceGroupName $rgName -Name $vnetName
+    $pip = Get-AzPublicIpAddress -ResourceGroupName $rgName -Name $pipName
+    $azFw.Allocate($vnet, $pip)
+    Set-AzFirewall -AzureFirewall $azfw
+  } else {
+    Write-Output "Firewall already allocated."
+  }
 }
