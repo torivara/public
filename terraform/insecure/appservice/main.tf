476821b666dc5af2a7dde9993943146d733e77f5
Tor Ivar <torivara@gmail.com>
2023-08-25 15:12:58 +0200

    update

--- terraform/insecure/appservice/main.tf
+++ terraform/insecure/appservice/main.tf
@@ -9,7 +9,7 @@ resource "azurerm_resource_group" "rg" {
 
 resource "azurerm_service_plan" "plan" {
   location            = azurerm_resource_group.rg.location
-  name                = "tf-insecure-app-plan"
+  name                = "tf-insecure-app01-plan"
   os_type             = "Linux"
   resource_group_name = azurerm_resource_group.rg.name
   sku_name            = "F1"
@@ -19,7 +19,7 @@ resource "azurerm_service_plan" "plan" {
 # Should support windows_app_service and not only the deprecated resource type
 resource "azurerm_app_service" "app" {
   location            = azurerm_resource_group.rg.location
-  name                = "tf-insecure-app-as"
+  name                = "tf-insecure-app01-as"
   resource_group_name = azurerm_resource_group.rg.name
   service_plan_id     = azurerm_service_plan.plan.id
   site_config {
